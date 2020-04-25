d35ed4d71bc1b85dae6e5eddad95299db9bd144f
EvanCharles16 <evancharles1609@gmail.com>
2020-04-25 19:11:43 +0700

    Learn-react-redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Libraries from "./components/Libraries";
+import Students from './components/Students';
+
+import Navbar from "./components/Navbar";
+
+import reducers from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="container">
+          <Navbar />
+          <div className="mt-4">
+            <Switch>
+              <Route path="/libraries">
+                <Libraries />
+              </Route>
+              <Route path="/students">
+                <Students />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
