64e215e0feda0cd64abe36d7c59b44454031f999
EvanCharles16 <evancharles1609@gmail.com>
2020-04-28 02:08:45 +0700

    initial project

--- src/components/Libraries/Main.js
+++ src/components/Libraries/Main.js
@@ -9,7 +9,9 @@ import Add from "./Add";
 import Delete from "./Delete";
 import Edit from "./Edit";
 import Item from "./Item";
+import bg from "../../img/bg.png";
 
+import banner from "../../img/banner.png";
 import kiri from "../../img/kiri.png";
 import kanan from "../../img/kanan.png";
 
@@ -20,7 +22,36 @@ const Main = (props) => {
 
   return (
     <div>
-      <Row>
+      <div style={{ position: "absolute", top: "7%", left: "0" }}>
+        <img
+          src={bg}
+          style={{
+            width: "100%",
+            height: "30rem",
+            position: "relative",
+          }}
+        />
+        <div style={{ position: "absolute", top: "20%", left: "24%" }}>
+          <h1 style={{ borderBottom: "2px solid black", width: "30rem" }}>
+            " Welcome To My Library "
+          </h1>
+          <h5 className="mt-4" style={{ position: "absolute" }}>
+            Education is what remains after one has forgotten what one has
+            learned in School
+          </h5>
+        </div>
+        <img
+          src={banner}
+          alt="banner"
+          style={{
+            width: "26rem",
+            position: "absolute",
+            top: "10%",
+            left: "56%",
+          }}
+        />
+      </div>
+      <Row className="fixed-bottom  ">
         <Col md={2}>
           <img src={kiri} style={{ width: "20rem", position: "absolute" }} />
         </Col>
